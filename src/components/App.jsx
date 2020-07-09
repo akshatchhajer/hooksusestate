d2e66500f6a20eb8c043f3b4a6eecf138b6d4dc6
akshatchhajer <65116361+akshatchhajer@users.noreply.github.com>
2020-07-10 01:41:06 +0530

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Change from "./change";
+
+function App() {
+  return (
+    <div>
+      <h1>hello</h1>
+      <Change />
+    </div>
+  );
+}
+
+export default App;
